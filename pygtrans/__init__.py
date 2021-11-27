f09260036390d61122345ee162c43381099a4738
bob <1478449280@qq.com>
2021-11-27 21:12:30 +0800

    删除 api key 测试

--- pygtrans/__init__.py
+++ pygtrans/__init__.py
@@ -1,18 +1,16 @@
 """谷歌翻译"""
 
-from .ApiKeyTranslate import ApiKeyTranslate, split_list_by_content_size
+from .ApiKeyTranslate import ApiKeyTranslate
 from .DetectResponse import DetectResponse
 from .LanguageResponse import LanguageResponse
 from .Null import Null
 from .Translate import Translate
 from .TranslateResponse import TranslateResponse
 
-__apikey__ = '<your-api-key>'
-
 __title__ = 'pygtrans'
 __description__ = 'Google Translate, support APIKEY'
 __url__ = 'https://github.com/foyoux/pygtrans'
-__version__ = '1.1.1'
+__version__ = '1.2.0'
 __author__ = 'foyou'
 __author_email__ = 'yimi.0822@qq.com'
 __license__ = 'GPL-3.0'
@@ -20,6 +18,5 @@ __copyright__ = f'Copyright 2021 {__author__}'
 __ide__ = 'PyCharm - https://www.jetbrains.com/pycharm/'
 
 __all__ = [
-    'Translate', 'ApiKeyTranslate', 'Null', 'LanguageResponse', 'DetectResponse', 'TranslateResponse',
-    'split_list_by_content_size'
+    'Translate', 'ApiKeyTranslate', 'Null', 'LanguageResponse', 'DetectResponse', 'TranslateResponse'
 ]
